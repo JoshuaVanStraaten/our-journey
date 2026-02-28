7ea742b4e19b999bcac382d7aedb026057a63e40
Joshua Van Straaten <joshuavanstraaten100@gmail.com>
2026-02-28 17:03:12 +0200

    refactor: Redo the entire site
    Signed-off-by: Joshua Van Straaten <joshuavanstraaten100@gmail.com>

--- data/config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-  relationshipStart: "2026-01-29T00:00:00",
-
-  typingText: ["This is our story.", "Written by God.", "Lived by us."],
-};
--- js/typing.js
+++
@@ -1,8 +0,0 @@
-function startTyping() {
-  new Typed("#typed", {
-    strings: CONFIG.typingText,
-    typeSpeed: 50,
-    backSpeed: 0,
-    showCursor: false,
-  });
-}
